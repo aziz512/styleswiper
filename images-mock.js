a2799e5909f48927566a001b69ceb1cf9852e1e5
Aziz Yakubjanov <yakubjanov0@hotmail.com>
2016-05-21 16:36:53 +0000

    main files

---
+++ images-mock.js
@@ -0,0 +1,16 @@
+var images = [{
+    name : "js",
+    src : "https://instagram.fsjc1-2.fna.fbcdn.net/t51.2885-15/e35/13249687_2033717946853487_117888574_n.jpg?ig_cache_key=MTI1NDk3MjkzMTE3MDU2NzA4Mg%3D%3D.2,"
+},
+{
+    name : "js1",
+    src : "https://instagram.fsjc1-2.fna.fbcdn.net/t51.2885-15/e35/13129445_1019750264782536_1505860263_n.jpg?ig_cache_key=MTI1MzcyNzA4NzIzMTMyMjIyMA%3D%3D.2",
+},
+{
+    name : "js2",
+    src : "https://instagram.fsjc1-2.fna.fbcdn.net/t51.2885-15/e35/13256702_1011741675569428_1416150466_n.jpg?ig_cache_key=MTI1NDY1OTM5NDQyMzMxMDY3Mg%3D%3D.2",
+},
+{
+    name : "js3",
+    src : "https://instagram.fsjc1-2.fna.fbcdn.net/t51.2885-15/e35/13116604_1053276324742602_132546166_n.jpg?ig_cache_key=MTI1NDczNDc3MzMxMzk0ODA5Mg%3D%3D.2",
+}]
---
+++ selected.js
@@ -0,0 +1,3 @@
+for (var number = 0; number < images.length; number = number + 1) {
+console.log(images[number]);    
+}
